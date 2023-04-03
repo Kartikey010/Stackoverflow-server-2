2268d3f983b050c94820ffd9cd0f262c34aa9ba6
Kartikey <kartikey000k@gmail.com>
2023-04-03 17:01:36 +0530

    timechange

--- index.js
+++ index.js
@@ -229,8 +229,8 @@ const nextUpdate = new Date(
   now.getFullYear(),
   now.getMonth(),
   now.getDate() ,
-  11, // 12:00 am
-  51 // 0 minutes past the hour
+  17, // 12:00 am
+  03 // 0 minutes past the hour
 );
 
 const timeUntilNextUpdate = nextUpdate.getTime() - now.getTime();
